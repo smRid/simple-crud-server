49a51a6ae99ddd480eef7a029f4532d21e8007a0
smRid <https://github.com/smRid/Arena-Chatbot.git>
2026-05-18 13:51:17 +0600

    add POST, PATCH, and DELETE endpoints for user management

--- index.js
+++ index.js
@@ -1,63 +1,107 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+
 const uri = `mongodb+srv://simpleCrudUser:2MXgqnxH3bLoctnh@cluster0.vyznij5.mongodb.net/?appName=Cluster0`;
 
+// mongodb+srv://simpleCrudUser:2MXgqnxH3bLoctnh@cluster0.vyznij5.mongodb.net/?appName=Cluster0
+// mongodb+srv://<db_username>:<db_password>@cluster0.vyznij5.mongodb.net/?appName=Cluster0
+
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 const run = async () => {
-  try {
-    await client.connect();
+    try {
+        await client.connect();
 
-    const db = client.db("simpleCrud");
-    const userCollection = db.collection("users");
+        const db = client.db('simpleCrud');
+        const userCollection = db.collection('users');
 
-    app.get("/users", async (req, res) => {
-      const cursor = userCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    });
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
 
-    app.get("/users/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = {
-        _id: new ObjectId(id),
-      };
-      const user = await userCollection.findOne(query);
-      console.log("user id", id);
-      res.send(user);
-    });
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const user = await userCollection.findOne(query)
+            console.log('user id', id)
+            res.send(user);
+        })
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!",
-    );
-  } finally {
-    // await client.close();
-  }
-};
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            // console.log('user to be inserted', newUser)
+            const result = await userCollection.insertOne(newUser);
+
+            res.send(result);
+        })
+
+        app.patch('/users/:id', async(req, res) =>{
+            const id = req.params.id;
+            const filter = {
+                _id: new ObjectId(id)
+            }
+            const modifiedUser = req.body;
+
+            const updatedDocument = {
+                $set: {
+                    name: modifiedUser.name,
+                    email: modifiedUser.email,
+                    role: modifiedUser.role
+                }
+            }
+
+            const result = await userCollection.updateOne(filter, updatedDocument);
+
+            res.send(result);
+
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+        await client.db('admin').command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+        // await client.close();
+    }
+}
 
 run().catch(console.dir);
 
-app.get("/", (req, res) => {
-  res.send("Simple CRUD server is serving");
-});
+
+app.get('/', (req, res) => {
+    res.send('Simple CRUD server is serving');
+})
 
 app.listen(port, () => {
-  console.log(`Simple CRUD server is running on port ${port}`);
-});
+    console.log(`Simple CRUD server is running on port ${port}`);
+})
+
 
 // try {
 
