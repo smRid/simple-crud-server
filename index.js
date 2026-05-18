1c8a750cac7045e135e982e1dba005b493e910d6
smRid <https://github.com/smRid/Arena-Chatbot.git>
2026-05-18 13:47:05 +0600

    add users route to get users data

--- index.js
+++ index.js
@@ -1,59 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://simpleCrudUser:2MXgqnxH3bLoctnh@cluster0.vyznij5.mongodb.net/?appName=Cluster0`;
 
-// mongodb+srv://simpleCrudUser:2MXgqnxH3bLoctnh@cluster0.vyznij5.mongodb.net/?appName=Cluster0
-// mongodb+srv://<db_username>:<db_password>@cluster0.vyznij5.mongodb.net/?appName=Cluster0
-
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 const run = async () => {
-    try {
-        await client.connect();
+  try {
+    await client.connect();
 
-        const db = client.db('simpleCrud');
-        const userCollection = db.collection('users');
+    const db = client.db("simpleCrud");
+    const userCollection = db.collection("users");
 
-        app.get('/users', async(req, res) =>{
-            const cursor = userCollection.find();
-            const result = await cursor.toArray();
-            res.send(result);
-        })
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-
-        await client.db('admin').command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }
-    finally {
-        // await client.close();
-    }
-}
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+};
 
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('Simple CRUD server is serving');
-})
+app.get("/", (req, res) => {
+  res.send("Simple CRUD server is serving");
+});
 
 app.listen(port, () => {
-    console.log(`Simple CRUD server is running on port ${port}`);
-})
-
+  console.log(`Simple CRUD server is running on port ${port}`);
+});
 
 // try {
 
